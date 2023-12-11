b0f51df1988e16b358c4f6a230fd37d1b518ce50
Alves15 <rafaelalves@vivaldi.net>
2023-12-11 17:03:51 -0300

    added progress on this exercise. Add, subtract, sum, multiply and power functions are done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {  
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const total = a.reduce((sum, current) => sum + current, 0);
+  return total;
+}
+
+const multiply = function(...a) {
+  const total = a.reduce((sum, current) => sum * current, 1);
+  return total;
 };
 
-const multiply = function() {
+const power = function(a, b) {
+  d = a * a;
+            
+  for (let c = 2; c < b; c++) {
+      d = d * a;
+      c++;   
+  }
 
-};
-
-const power = function() {
-	
+  return d;	          
 };
 
 const factorial = function() {
