e0108aff637f23abefc485d0062fad8b7775e49e
Alves15 <rafaelalves@vivaldi.net>
2023-12-11 17:00:13 -0300

    added exercise05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
+                
 
+    if (initial < 0 || final < 0){
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(initial) || !Number.isInteger(final)){
+        return "ERROR";
+    }
+
+    if (initial > final){
+        l = final;
+        final = initial;
+        initial = l;
+        n = initial;
+        steps = initial;
+        add = 2;
+
+        while (steps != final) {
+            n += add;
+            ++add;
+            ++steps;
+        }
+    }else {
+        n = initial;
+        steps = initial;
+        add = 2;
+
+        while (steps != final) {
+        n += add;
+        ++add;
+        ++steps;
+    }
+
+    
+    }
+
+    return n;
+    
+    
 };
+
+sumAll(1,4);
 
 // Do not edit below this line
 module.exports = sumAll;
