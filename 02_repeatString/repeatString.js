dc70bf62138f3de83652662756eaddd7bc7ad3f6
Alves15 <rafaelalves@vivaldi.net>
2023-12-11 16:56:41 -0300

    added exercise02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    n = 0;
+    result = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    while (n < num) {
+        result += string;
+        ++n;
+    }
 
+    return result;
 };
+
+repeatString('hey', 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
