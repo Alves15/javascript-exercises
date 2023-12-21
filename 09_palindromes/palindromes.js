94011d897a24005d6560ae03cdb203a1cc8c5d09
Alves15 <rafaelalves@vivaldi.net>
2023-12-20 22:40:37 -0300

    removed trim, use of replaceAll to remove punctuation and whitespace in string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,14 @@
 const palindromes = function (word) {             
         let test = word;
         test = test.toLowerCase();
+        test = test.replaceAll(/\!|\?|\,|\.|" "/g,'');
         let palin = "";
         let result = test.split("");
         let reversed = result.reverse();
         console.log(reversed);
         for (i = 0; i < reversed.length; i++) {
             palin += reversed[i];
-        }
-        palin = palin.replaceAll(/!/g,'');
-        test = test.replaceAll(/!/g,'');
+        }      
         if (test == palin){
             return true;
         }else{
